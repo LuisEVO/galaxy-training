2dacbbb514ae52c8968031a5f1b99b27e695afce
Luis Vilcarromero <luis.evo_8.10@gmail.com>
2019-05-25 14:44:34 -0500

    cords added

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const workshopRoutes = require('./api/routes/workshop');
 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+        return res.status(200).json({})
+    }
+    next();
+});
 app.use('/workshop', workshopRoutes);
 
 module.exports = app;
